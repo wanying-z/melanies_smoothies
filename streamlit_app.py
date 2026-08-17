3d10d1b602341f8cee02cd32808ca2a4c5af4cbf
wanying-zhu <114609057+wanying-z@users.noreply.github.com>
2026-08-17 14:33:09 +0000

    remove last change

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,6 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-session.sql("USE WAREHOUSE COMPUTE_WH").collect()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
