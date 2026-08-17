eca6350e738a4095416622d59dd67e42b5323a24
wanying-zhu <114609057+wanying-z@users.noreply.github.com>
2026-08-17 15:43:49 +0000

    Call API from SniS App

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
     
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
